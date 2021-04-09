fe19d2a616ce7a52407e687227d23a8f8f9c700e
fyc132 <286751251@qq.com>
2021-04-09 19:36:15 +0800

    fix KiTimerDispatch and KiDpcDispatch entry2
    fix KiTimerDispatch and KiDpcDispatch entry2

--- EasyAntiPatchGuard/DriverEntry.cpp
+++ EasyAntiPatchGuard/DriverEntry.cpp
@@ -1,7 +1,7 @@
 #include <ntifs.h>
 #include <ntimage.h>
 #include <intrin.h>
-
+#define uint64_t unsigned long long
 #define uint32_t unsigned long
 #define uint16_t unsigned short
 
@@ -32,7 +32,7 @@ bool easy_anti_patchguard(uintptr_t search_base) {
 	const auto sections_array = reinterpret_cast<PIMAGE_SECTION_HEADER>(reinterpret_cast<PUCHAR>(pNtHeader) + sizeof(
 		IMAGE_NT_HEADERS));
 
-	uint32_t KiTimerDispatch[2]{0};
+	uint32_t KiTimerDispatch[3]{0};
 
 	for (auto i = 0; i < section_size; i++) {
 		//INITKDBG
@@ -47,10 +47,10 @@ bool easy_anti_patchguard(uintptr_t search_base) {
 					if (const auto runtime_table = RtlLookupFunctionEntry(start_address, &image_base, 0)) {
 						const auto va = image_base + runtime_table->BeginAddress;
 						if (start_address - va <= 0x20) {
-							
+
 							KiTimerDispatch[0] = *(uint32_t*)(va);
 							KiTimerDispatch[1] = *(uint32_t*)(va + sizeof(uint32_t));
-
+							KiTimerDispatch[2] = *(uint32_t*)(va + sizeof(uint32_t) * 2);
 							DbgPrint("KiTimerDispatch :%p \n", va);
 							break;
 						}
@@ -77,32 +77,36 @@ bool easy_anti_patchguard(uintptr_t search_base) {
 				if (p1 == 0x1D8B4C && p2 == 0xC0854800) {
 					const auto _guard_icall_handler = scan_base + i;
 					DbgPrint("_guard_icall_handler:%p\n", _guard_icall_handler);
-					
+
 					/*
 					cmp     dword ptr ds:[rax], 0x1131482E   //     xor     qword ptr cs:[rcx],rdx
 					je      ret
-					cmp     qword ptr ds:[rax], 0x48513148   //     KiDpcDispatch 
+					cmp     dword ptr ds:[rax], 0x48513148   //     KiDpcDispatch
 					je      ret
-					cmp     qword ptr ds:[rax], 0x11111111   //		KiTimerDispatch[0]
+					cmp     dword ptr ds:[rax], 0x11111111   //		KiTimerDispatch[0]
 					jne     ok
 					cmp     qword ptr ds:[rax+0x4], 0x22222222  //	KiTimerDispatch[1]
+					jne     ok
+					cmp     qword ptr ds:[rax+0x8], 0x33333333  //	KiTimerDispatch[2]
 					jne     ok
 					ret
 					jmp     rax
 					 */
-				
+
 					unsigned char patch_code[] = {
-						0x48, 0x81, 0x38, 0x2E, 0x48, 0x31, 0x11, 0x74, 0x1C, 0x48, 0x81, 0x38, 0x48, 0x31, 0x51, 0x48,
-						0x74, 0x13, 0x48, 0x81, 0x38, 0x11, 0x11, 0x11, 0x11, 0x75, 0x0B, 0x48, 0x81, 0x78, 0x04, 0x22,
-						0x22, 0x22, 0x22, 0x75, 0x01, 0xC3, 0xFF, 0xE0
+						0x81, 0x38, 0x2E, 0x48, 0x31, 0x11, 0x74, 0x19, 0x81, 0x38, 0x48, 0x31, 0x51, 0x48, 0x74,
+						0x11,
+						0x81, 0x38, 0x11, 0x11, 0x11, 0x11, 0x75, 0x0A, 0x81, 0x78, 0x04, 0x22, 0x22, 0x22, 0x22,
+						0x75,
+						0x01, 0x81, 0x78, 0x08, 0x22, 0x22, 0x22, 0x22, 0x75, 0xF8, 0xC3, 0xFF, 0xE0
 					};
-					*(uint32_t*)(&patch_code[0x15]) = KiTimerDispatch[0];
-					*(uint32_t*)(&patch_code[0x1F]) = KiTimerDispatch[1];
-					
+					*(uint32_t*)(&patch_code[0x12]) = KiTimerDispatch[0];
+					*(uint32_t*)(&patch_code[0x1B]) = KiTimerDispatch[1];
+					*(uint32_t*)(&patch_code[0x24]) = KiTimerDispatch[2];
 					const auto irql = __readcr8();
 					KeRaiseIrqlToDpcLevel();
 					__writecr0(__readcr0() & 0xfffffffffffeffff);
-					
+
 					memcpy(_guard_icall_handler, patch_code, sizeof(patch_code));
 
 					__writecr0(__readcr0() | 0x10000);
